2658c7da71b47bc46c06eafebeeb085a4de88c29
Tyuiu.KomanichRM@outlook.com <gorotro_215@mail.ru>
2023-11-19 13:09:17 +0500

    Task2

--- Tyuiu.KomanichRM.Sprint5.Task2.V17.Test/DataServiceTest.cs
+++ Tyuiu.KomanichRM.Sprint5.Task2.V17.Test/DataServiceTest.cs
@@ -1,9 +1,9 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using Tyuiu.KomanichRM.Sprint5.Task1.V28.Lib;
+using Tyuiu.KomanichRM.Sprint5.Task2.V17.Lib;
 using System.IO;
 
-namespace Tyuiu.KomanichRM.Sprint5.Task1.V28.Test
+namespace Tyuiu.KomanichRM.Sprint5.Task2.V17.Test
 {
     [TestClass]
     public class DataServiceTest
@@ -11,7 +11,7 @@ namespace Tyuiu.KomanichRM.Sprint5.Task1.V28.Test
         [TestMethod]
         public void CheckedExistsFile()
         {
-            string path = @"C:\Users\rimch\source\repos\Tyuiu.KomanichRM.Sprint5\Tyuiu.KomanichRM.Sprint5.Task1.V28\bin\Debug\OutPutFileTask1.txt";
+            string path = @"C:\Users\rimch\source\repos\Tyuiu.KomanichRM.Sprint5\Tyuiu.KomanichRM.Sprint5.Task2.V17\bin\Debug\OutPutFileTask2.csv";
             FileInfo fileInfo = new FileInfo(path);
             bool fileExist = fileInfo.Exists;
             bool wait = true;
---
+++ Tyuiu.KomanichRM.Sprint5.Task2.V17/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KomanichRM.Sprint5.Task2.V17.Lib;
+
+namespace Tyuiu.KomanichRM.Sprint5.Task2.V17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Команич Р. М. | АСОиУб-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Запись структурированных данных в текстовый файл                  *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #17                                                             *");
+            Console.WriteLine("* Выполнил: Команич Роман Маркович | АСОиУб-23-3                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 3 на 3 элементов, заполненный        *");
+            Console.WriteLine("* значениями с клавиатуры. Заменить нечетные элементы массива на 0.       *");
+            Console.WriteLine("* Результат сохранить в файл OutPutFileTask2.csv и вывести на консоль.    *");
+            int[,] mtrx = new int[3, 3] { { 2, 1, 7 },
+                                          { 1, 2, 4 },
+                                          { 2, 3, 4 } };
+            int rows = mtrx.GetUpperBound(0) + 1;
+            int colums = mtrx.Length / rows;
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Массив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < colums; j++)
+                {
+                    Console.Write($"{mtrx[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            string res = ds.SaveToFileTextData(mtrx);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
